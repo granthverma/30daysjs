6550880c8490f0888e3c31ecfb28575dfe63470d
granthverma <himanshuvermanewtonsch@gmail.com>
2022-10-30 11:05:16 +0530

    learn about array literals  constructor switch case

---
+++ index3.js
@@ -0,0 +1,15 @@
+//we talk about switch case 
+
+let user ="admin"
+
+switch (user) {
+    case"admin":
+    console.log ("You are admin");
+    break;
+    case "student":
+        console.log ("You are Student")
+        break;
+        default:
+            console.log ("You are Trial user ") ;
+}
+console.log("Switch Case Complete") ;
---
+++ index4a.js
@@ -0,0 +1,23 @@
+// //array
+// //array literals
+// let Arr1 =["grath " ,"honey" ,"do it" ,"as free like bird"];
+// console.log (Arr1) ;
+// console.log(Arr1.length);
+// //1 ,2 ,3 ,4 
+// console.log(Arr1[Arr1.length -1 ]);
+// Arr1[3] ="granth.codes"
+// console.log(Arr1);
+//array constructor
+//dd
+// // let newarr = new Array ("a" ,"b" ,"c");
+// // console.log (newarr );
+
+ let Arr1 =["honey " , "chavi","steve" , "granth"];
+ console.log (Arr1);
+ Arr1.push ("himanshu");
+ console.log (Arr1);
+// // method of slice 
+// //Syntax:array.slice (startindex ,endindex-1)
+console.log (Arr1.slice(1,2));
+let Arr2 = Arr1.slice(1,1);
+ console.log (Arr2);
