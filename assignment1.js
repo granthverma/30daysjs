28cbb62305e822860b3e2f612204f7663fecdb2c
granthverma <himanshuvermanewtonsch@gmail.com>
2022-10-30 21:55:53 +0530

    assignment user signup

---
+++ assignment1.js
@@ -0,0 +1,31 @@
+// assignment user signup with every details
+const uid ="ashyd12345";
+firstName = "Granth";
+lastName ="Verma";
+cityName ="ajmer";
+stateName= "rajasthan";
+phoneNum = 7014257890;
+email = "granthverma.com"
+var password = "123456";
+var confirmPassword ="123456";
+level ="beginner"
+skill ="html css js wordpress";
+
+
+
+console.log (
+    `
+     Unique ID is : ${uid};
+     User first Name : ${firstName};
+     User last Name : ${lastName};
+     City Name : ${cityName};
+     State Name :${stateName};
+     User  Phone Num :${phoneNum};
+     and his email is : ${email};
+     and Use the password : ${password};
+     and User Level : ${level};
+     and He Have skills  :${skill};
+
+
+    `
+);
