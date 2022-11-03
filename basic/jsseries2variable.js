b1552f8e94e8d2fdaf1acf904752f89f95383c89
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-03 21:11:36 +0530

    initialcommit

---
+++ basic/jsseries2variable.js
@@ -0,0 +1,17 @@
+var fullName =" Granth Verma"
+
+/// camel case used uT
+var myAim ="world top 10 front end developer"
+var isLoggenIn = true ; // boolen value
+var loggedCount =34 ; // number
+
+// console.log(loggedCount); // the give value 
+// console.log ("loggedCount") ; // give a string value 
+
+var paymentMode;
+// console.log(paymentMode); // give undefined
+paymentMode ="Credit Card";
+console.log(paymentMode); // give-> credit card 
+
+
+ 
