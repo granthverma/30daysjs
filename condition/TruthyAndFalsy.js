0dc34a9453b5869fdea4712d34f65945b3abd4a7
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-03 21:40:44 +0530

    strict equality operator (===) & ==

---
+++ condition/TruthyAndFalsy.js
@@ -0,0 +1,53 @@
+// undefined
+// null  ///  empty value
+// 0 
+// ' '
+// NaN  // Not a Number 
+
+// /// above the value apart all are truthy value 
+
+
+// var user ;    ////------> underfined 
+
+
+// var user = "null" ;
+
+// if (user) {
+//     console.log ("Condition is true")
+// };
+
+   ////     when you add number & number ////
+
+
+
+// console.log(2+2);  // number +number -- > give you a number 
+// console.log(2+"2"); // number + string ---> give you a number+string 
+// console.log("2"+2); //string + number --- > give you a string +number 
+
+
+
+///   now we talk about  (  ==   ) double equal
+// you want more  know then see exl ---->
+
+// var user = "2" ;
+
+// if (2== user) {
+//     console.log ("Condition is true")
+// };
+
+
+/// what you got --->    Condition is true
+
+
+
+/// now if  we put ==== what is next now 
+
+
+var user = "2" ;
+
+if (2=== user ) {
+    console.log ("Condition is true")
+};
+  //?????   what we got  any gusses   ???
+  //  nothing  bec === check value and type both things  together
+// The strict equality operator ( === ) checks whether its two operands are equal,
