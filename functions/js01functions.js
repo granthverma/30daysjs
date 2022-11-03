74f60baf528a1997bd7889e482e4ff4c5d1b19c2
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-03 22:14:52 +0530

    Basics of functions in JavaScript

---
+++ functions/js01functions.js
@@ -0,0 +1,30 @@
+//  basic syntax of function
+
+// function name(params) {
+    
+// }
+
+
+function sayHello (name) {
+   console.log("Hello there , Granth.codes"); 
+//    console.log("Hello there  " , name);  old way
+   console.log(`hello there, ${name} .How are you`);
+}
+
+// sayHello("himanshu");
+// sayHello("granth");
+
+// Hello there , Granth.codes
+// Hello there   undefined
+
+// sayHello; // for calling function we use ()
+
+function namstey () {
+    return "Hello in India"
+}
+
+var greetings = namstey ();
+console.log (greetings);
+
+//  return calling the functions 
+
