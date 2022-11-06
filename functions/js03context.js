d19645d8cdeafa3d625425d57fb5adfa549f9b0f
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-06 22:15:40 +0530

    initial commit

---
+++ functions/js03context.js
@@ -0,0 +1,20 @@
+// sayHello
+
+
+// function sayHello() {
+//     console.log("hello");
+    
+// }
+
+
+if (2==="2") {
+  console.log("This is true");  
+} 
+
+
+var myName ="granth .codes"  ;
+
+if (myName ===myName ) {
+    console.log("this is again a true statements");
+    
+}
