9e1fe7f6aac11f8b9b2b46f9e40a2e38bdf83ac3
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-06 22:50:36 +0530

    Function declarations are scanned and made available vaiable declarations and made undefined

---
+++ functions/js04hoisting.js
@@ -0,0 +1,30 @@
+
+tipper(80);
+function tipper(a) {
+    
+    var bill = parseInt (a) ;
+    console.log(bill+5);
+}
+
+bigTipper("200")
+
+var bigTipper =function (a) {
+    var bill = parseInt (a);
+    console.log(bill +14);
+    
+};
+
+
+// Function declarations are scanned and made available
+// vaiable declarations and made undefined 
+
+
+//---------------  CONTEXT   -------------///
+
+       //..................................
+
+
+    //    EXECUTION CONTEXT
+    // . VARIABLE OBJECT 
+    //. SCOPE CHAIN 
+    //. THIS 
