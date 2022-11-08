7d511caa9cee076747c458b48e1ef1ddfc55c752
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-08 08:57:06 +0530

    Learn about array

---
+++ Array/01array.js
@@ -0,0 +1,22 @@
+
+// how array declare 
+
+var countries =[ "India ","Usa" , "dubai" , "Japan"]; // old way
+var states = new Array ("Rajasthan" ,"delhi","Mumbai", "Assam"); // new way
+///access this value  --------------->>>>>>>
+
+// array start from 0 
+
+console.log(states[0]);
+
+console.log(countries);
+
+/// length of array
+
+console.log(states.length);
+
+
+states [0] ="Punjab" ;
+console.log(states);
+
+
