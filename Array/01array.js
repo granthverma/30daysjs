f8571042448de5d6664cea20792351e756616c39
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-08 09:10:12 +0530

    learn about shift & unshift , pop

--- Array/01array.js
+++ Array/01array.js
@@ -7,16 +7,34 @@ var states = new Array ("Rajasthan" ,"delhi","Mumbai", "Assam"); // new way
 
 // array start from 0 
 
-console.log(states[0]);
+// console.log(states[0]);
 
-console.log(countries);
+// console.log(countries);
 
-/// length of array
+// /// length of array
 
-console.log(states.length);
+// console.log(states.length);
+
+// // you want change states 
+// states [0] ="Punjab" ;
+// console.log(states);
 
 
-states [0] ="Punjab" ;
-console.log(states);
+var user = ["honey " ,"granthcodes@com",3 ,34 ,true];
+console.log(user);
+// remove the value we pop 
+// pop remove the value from end
+user.pop() ;
+console.log(user);
 
 
+/// Unshift the value  -- unshift shift value from end
+
+//
+
+user.unshift ("NEW VALUE") ;
+console.log (user);
+user.shift();
+console.log(user);
+
+//
