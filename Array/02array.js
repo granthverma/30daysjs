68cdafbd4a73ecab15fab9c36b97ea91d51f4a9f
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-09 23:59:47 +0530

    return the value

--- Array/02array.js
+++ Array/02array.js
@@ -26,5 +26,11 @@ var isEven = (element) => {
 
 
 
-var result = [2, 4, 6,8].every((e)=> (e % 2 ===0 ) ) ;
+// var result = [2, 4, 6,8].every((e)=> (e % 2 ===0 ) ) ;
+// console.log(result);   
+
+var result = [2, 4, 6,8].every((e) => {
+    return (e % 2 ===0 )
+}
+ ) ;
 console.log(result);
