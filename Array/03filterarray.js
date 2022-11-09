4b04db37a88fed4f4aaaf65f5130b4b1a01cd6da
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-10 00:22:28 +0530

    filter keyword

--- Array/03filterarray.js
+++ Array/03filterarray.js
@@ -5,3 +5,9 @@ var testArray =[2 ,3 ,4 ,6, 7,8, 9,10] ;
 console.log(testArray.fill("zero" ,2 ,5));
 
 /// (value , start , end)
+
+ const myNumber =[23 , 24, 25, 55,66, 77, 87, 98];
+
+ const result = myNumber.filter((num) => num <55 );
+
+ console.log(result);
