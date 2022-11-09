4a3993ea57c61718217294767de5a4618a6929b6
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-10 00:01:21 +0530

    initial commit

--- Array/02array.js
+++ Array/02array.js
@@ -24,13 +24,17 @@ var isEven = (element) => {
 // () => {}    when we use {} then return value 
 // () => ()    ---   without return the value
 
-
+           ///  without return the value  //
 
 // var result = [2, 4, 6,8].every((e)=> (e % 2 ===0 ) ) ;
 // console.log(result);   
 
+
+
+              /// when we use {} then return value 
+              
 var result = [2, 4, 6,8].every((e) => {
     return (e % 2 ===0 )
 }
  ) ;
-console.log(result);
+console.log(result);  
