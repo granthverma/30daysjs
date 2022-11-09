299bc34fee29d7a0d7dc7c61ee58919497327cdd
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-09 23:49:20 +0530

    every fun

--- Array/02array.js
+++ Array/02array.js
@@ -1,8 +1,16 @@
-function isEven(element) {
-    if (element%2 === 0){
-        return true ;
-    }
-    return false;
+// var isEven =function (element)
+
+// arrow function 
+
+var isEven = (element) => {
+    // if (element%2 === 0){
+    //     return true ;
+    // }
+    // return false;
+    return element% 2 === 0;
 }
 
-console.log(isEven(2));
+// console.log(isEven(2));
+
+var result = [2, 4, 6,8].every(isEven);
+console.log(result);
