8d0ec60a51c9a2d2be19e562937fa5afdc613abf
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-11 23:59:05 +0530

    while do while loops

--- Array/06loops.js
+++ Array/06loops.js
@@ -9,11 +9,29 @@ const myCity = ["Ajmer" ,
                 "Jaipur",
                 1999, 
                "Kota",
-              "Mumbai" ]
+              "Mumbai" ] ;
 
- for (let i =0 ; i< myCity.length; i++ ) {
-    // console.log(i); // its give length of array 
-    if (typeof myCity[i] !== "string") continue
-    console.log(myCity [i]);
- }
+//  for (let i =0 ; i< myCity.length; i++ ) {
+//     // console.log(i); // its give length of array 
+//     if (typeof myCity[i] !== "string") continue
+//     console.log(myCity [i]);
+//  }
 
+
+    // while loops 
+// let i = 0;
+
+// while (i < myCity.length) {
+//   console.log(myCity[i]);
+//     i++;
+// };
+
+//{ scope }  break continue  i++ don't forgets this 
+
+
+//  do while loops
+let i = 20;
+do {
+     console.log(i);
+     i++;
+   }while (i < 10) ;
