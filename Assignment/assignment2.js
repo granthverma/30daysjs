0572e4450686606e371043f726f97e61fc7f4623
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-11 09:08:53 +0530

    assignment2

---
+++ Assignment/assignment2.js
@@ -0,0 +1,13 @@
+var iphone = {
+     Model : "14 Pro Max",
+     Color : "Deep Purple ",
+     Price : 119000,
+     Display : " 6.7 Super Retina XDR display footnote" ,
+     Storage: "256" ,
+     Bionicchip : "A16 " ,
+     BatteyBackup : "26 Hours playback time ",
+     
+     
+
+}
+console.table(iphone);
