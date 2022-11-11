aa1512fa2045d38c655da0d3bd38b449d91896dd
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-12 00:47:38 +0530

    For Each Loop

---
+++ Array/07loopstwo.js
@@ -0,0 +1,20 @@
+const myCity = ["Ajmer" , 
+                "Bangalore" ,
+                "Pune", 
+                "Jaipur",
+                1999, 
+               "Kota",
+              "Mumbai" ] ;
+   
+
+
+    //  var i = 0 ;
+
+    //  for (; ;) {
+    //     if (i >3) break ;
+    //     console.log(i);
+    //     i++;
+    //  }
+
+    myCity.forEach( (a) => (console.log(a)));
+
