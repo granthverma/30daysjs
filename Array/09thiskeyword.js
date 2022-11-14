4bf662e51245e55c90f119880218064a9a0cc234
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-14 08:52:00 +0530

    for all regular function calls , this points to window Object

--- Array/09thiskeyword.js
+++ Array/09thiskeyword.js
@@ -3,3 +3,19 @@
 
 
 console.log(this);
+var user = {
+    firstName :"Granth" ,
+    courseCount :4 ,
+    getCourseCount : function () {
+        console.log("Line 10" ,this );
+        function sayHello() {
+            console.log("Hello");
+            console.log("LINE 10", this);
+            
+        }
+        sayHello()
+    },
+
+} ;
+
+user.getCourseCount () ;
