22189fcbcc34cd33463784530791848a81ff399e
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-28 23:23:47 +0530

    LexicalScoping

--- NotAdvance/function.js
+++ NotAdvance/function.js
@@ -1,4 +1,21 @@
+// Anonymous Function 
+
 (function () {
     console.log("hello mr X");
     console.log("hello mr Y ");
 })();
+
+
+// golden rule functions programing 
+
+// 1 keep your data and keep your functionally totally separate 
+
+
+// 2 the same VAIABLE AGAIN & AGAIN YOUR INSTEAD DECLARED  
+// WITH DIFFENED NAME 
+// 3 FUNCTION ARE TREATED AS FIRSTCLASS 
+
+// Self Executing Anonymous Function and functional programming?
+
+// lexical enverment local memoery with the parents
+//
---
+++ NotAdvance/lexicalscoping03.js
@@ -0,0 +1,10 @@
+function init () {
+    var firstName = "granth" ;
+    function sayfirstName() {
+        console.log(firstName);
+        
+    }
+    sayfirstName ();
+}
+
+init ();
