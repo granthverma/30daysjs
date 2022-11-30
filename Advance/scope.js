1a9741e74e7bb01e1e09b8faf870e5d47738459a
granthverma <himanshuvermanewtonsch@gmail.com>
2022-11-30 08:41:13 +0530

    string

---
+++ Advance/scope.js
@@ -0,0 +1,14 @@
+//console.log(name);
+
+var name = "hitesh";
+
+if (true) {
+  let lastName = "Choudhary";
+}
+console.log(lastName);
+
+
+//let ke use modern world main  backend lau main hota ha 
+
+// scope end let end 
+// let give you errors its imporant
---
+++ Advance/string.js
@@ -0,0 +1,11 @@
+var name = "granth" ;
+// `` backteck  =  `granth\`s`` ;
+
+
+var fullName =`${name} verma` ;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("a"));
+console.log(fullName.includes("granth"));
+console.log(fullName.toUpperCase());
